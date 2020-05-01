1be55559c715df5e9558da430a59ad1edf453116
Jesús Mejía <jesusmejded@gmail.com>
2020-05-01 01:19:18 -0500

    bilevel optimization functions

---
+++ src/objective-functions.jl
@@ -0,0 +1,16 @@
+function F(x, y)
+        ids = y[:ids]
+        mean_y = mean(y[:y], dims=2)[:,1]
+        I = .!ids
+
+        m = mean(mean_y[ I ])
+        if isnan(m)
+            m = 0.0
+        end
+
+        10m #+ 1e2sum(I) + 0.1norm(x,1)
+end
+
+function f(x, y)
+    Float64(sum(y[:ids]))
+end
