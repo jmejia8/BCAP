fa005266366061bbb2bff4f52e5e236f5a6a0e18
Jesús Mejía <jesusmejded@gmail.com>
2020-05-01 01:17:17 -0500

    Initialize bilevel population

---
+++ src/initialize.jl
@@ -0,0 +1,38 @@
+function initialize!(problem,engine,parameters,status,information,options)
+    #########################################
+    ########### parameter setting ###########
+    #########################################
+    iterations = options.f_calls_limit ÷ (parameters.N * parameters.calls_per_instance * length(parameters.benchmark))
+    options.debug && @show iterations
+    if iterations < 5
+        iterations = 11
+        parameters.N =  options.f_calls_limit ÷ (iterations * parameters.calls_per_instance * length(parameters.benchmark))
+        @show parameters.N
+        parameters.K = 2
+    end
+
+    if parameters.N <=  parameters.K
+        error("Increase budget...")
+    end
+
+    #########################################
+
+    t1 = time()
+    BCAOperators.initialize!(problem,engine,parameters,status,information,options)
+    t2 = time()
+
+    status.final_time = t2
+
+    s = zeros(length(status.best_sol.y[:ids]))
+    for sol in status.population
+        s += sol.y[:ids]
+    end
+
+
+    t = round(Int, (t2 - t1) * (options.f_calls_limit - status.f_calls) / length(status.population))
+
+    options.debug && @printf("Remeaining time: %02dm%02ds\n", t ÷ 360, (t % 360) ÷ 60)
+
+
+
+end
