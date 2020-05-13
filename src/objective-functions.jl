948d6d47623b6a915a85fcd054ac69a9c27f3647
Jesus Mejia <jesusmejded@gmail.com>
2020-05-12 20:55:10 -0500

    Quadratic UL function

--- src/objective-functions.jl
+++ src/objective-functions.jl
@@ -2,12 +2,12 @@ function F(x, y)
         mean_y = mean(y.instance_values, dims=2)[:,1]
         not_solved = .!y.solved_instances
 
-        m = mean(mean_y[ not_solved ])
+        m = norm(mean_y[ not_solved ])
         if isnan(m)
             m = 0.0
         end
 
-        10m + 1e2sum(not_solved) + 0.1norm(x,1)
+        m + 0.1norm(x,1)
 end
 
 function f(x, y)
