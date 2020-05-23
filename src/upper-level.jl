ef9f14e11a6a4ccaa9c73ec7aa02f4687bfec3cf
Jesus Mejia <jesusmejded@gmail.com>
2020-05-23 00:15:11 -0500

    Minor changes

--- src/upper-level.jl
+++ src/upper-level.jl
@@ -1,8 +1,24 @@
-function update_state!(problem,engine,parameters,status,information,options,t_main_loop)
+function update_state!(
+    problem,
+    engine,
+    parameters,
+    status,
+    information,
+    options,
+    t_main_loop,
+)
 
     best = deepcopy(status.best_sol)
     P_old = copy(status.population)
-    Bilevel.BCAOperators.update_state!(problem,engine,parameters,status,information,options,t_main_loop)
+    Bilevel.BCAOperators.update_state!(
+        problem,
+        engine,
+        parameters,
+        status,
+        information,
+        options,
+        t_main_loop,
+    )
 
     if !status.best_sol.y.isfeasible || status.best_sol.F > best.F
         status.best_sol = best
@@ -18,7 +34,11 @@ function update_state!(problem,engine,parameters,status,information,options,t_ma
     reevaluate = status.stop || (t_main_loop > 0 && t_main_loop % 5 != 0)
 
     # force reevaluation of last population
-    force_reevaluation = options.f_calls_limit - status.f_calls <= (parameters.N)*length(parameters.benchmark) * parameters.calls_per_instance
+    force_reevaluation =
+        options.f_calls_limit - status.f_calls <=
+        (parameters.N) *
+        length(parameters.benchmark) *
+        parameters.calls_per_instance
 
 
     if reevaluate && !force_reevaluation
@@ -26,8 +46,9 @@ function update_state!(problem,engine,parameters,status,information,options,t_ma
         return
     end
 
-     options.debug && @info "Re-evualing...."
-     options.debug && force_reevaluation && @info "Forced Re-evualuation...."
+    options.debug && @info "Re-evualing...."
+    options.debug && force_reevaluation && @info "Forced Re-evualuation...."
+
 
 
     for sol in status.population
@@ -36,7 +57,14 @@ function update_state!(problem,engine,parameters,status,information,options,t_ma
 
             pred = sol.y.solved_instances
 
-            ll_result = engine.lower_level_optimizer(sol,problem,status,information,options, t_main_loop)
+            ll_result = engine.lower_level_optimizer(
+                sol,
+                problem,
+                status,
+                information,
+                options,
+                t_main_loop,
+            )
             status.f_calls += ll_result.f_calls
             q = ll_result.y
             FF = problem.F(p, q)
@@ -93,7 +121,7 @@ function stop_criteria(status, information, options)
         return true
     end
 
-    Fs = map(sol -> sol.F, status.population )
+    Fs = map(sol -> sol.F, status.population)
     status.stop = var(Fs) < 1e-8
     status.stop_msg = status.stop ? "var(F(Φ_i)) ≈ 0" : ""
 
@@ -105,7 +133,7 @@ end
 function final_stage!(status, information, options)
     i = 1
     ids = Int[]
-    for sol = status.population
+    for sol in status.population
         if sol.y.isfeasible && is_better(sol, status.best_sol)
             status.best_sol = sol
         end
@@ -117,7 +145,9 @@ function final_stage!(status, information, options)
         i += 1
     end
 
-    options.debug && length(ids) > 0 && @info "Removing $(length(ids)) infeasible solution(s)."
+    options.debug &&
+        length(ids) > 0 &&
+        @info "Removing $(length(ids)) infeasible solution(s)."
 
     deleteat!(status.population, ids)
 end
